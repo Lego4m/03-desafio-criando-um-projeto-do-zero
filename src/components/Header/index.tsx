2a56bf51f18566b83b47af5223a80fa06f0adea5
Leonardo Maron <leo22.maron@hotmail.com>
2022-05-21 22:25:51 -0300

    Componente: Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,19 @@
-export default function Header() {
-  // TODO
-}
+import Link from 'next/link';
+import commonStyles from '../../styles/common.module.scss';
+import styles from './header.module.scss';
+
+const Header: React.FC = () => {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.content}>
+        <Link href="/">
+          <a>
+            <img src="/Logo.svg" alt="logo" />
+          </a>
+        </Link>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
